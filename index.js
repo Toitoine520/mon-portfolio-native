7d7d354952ca7932e45be3cd4256ef91f8538ec4
Toitoine520 <alenagard57@gmail.com>
2025-07-30 15:43:35 +0200

    couleurs principales + bloc coloré test + début menu

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // document.addEventListener("load", () => {
 
 let zoneDeTexte = document.querySelector(".texte-auto-ecrit");
+let presentation = document.querySelector(".presentation > .contenu");
 
 let texteAEcrire = zoneDeTexte;
-autoWrite(texteAEcrire, zoneDeTexte.getAttribute("vitesse-ecriture"));
+autoWrite(texteAEcrire, zoneDeTexte.getAttribute("data-vitesse-ecriture"));
+autoWrite(presentation, 8);
 
 // });
 
 /**
  * Texte auto écrit
- * // 1 vitesse lente, 10 rapide
+ * | 1 vitesse lente, 10 rapide
  */
 function autoWrite(zoneDeTexte, vitesseEcriture) {
-    if (vitesseEcriture == undefined) vitesseEcriture = 5;
+  if (typeof zoneDeTexte != "object") {
+    throw new Error("Le type de l'élément n'est pas object");
+  }
+  if (!isANumber(vitesseEcriture)) {
+    throw new Error(vitesseEcriture + " devrait être un nombre");
+  }
+
+  if (vitesseEcriture == undefined) vitesseEcriture = 5;
+
   // decomposition de la string dans un tableau
   let texteDecompose = zoneDeTexte.innerText.split("");
   // nettoyage et remplissage de la zone de texte caractère par caractère
@@ -21,11 +31,29 @@ function autoWrite(zoneDeTexte, vitesseEcriture) {
   for (let i = 0; i < texteDecompose.length; i++) {
     let caractere = texteDecompose[i];
     setTimeout(() => {
-        // bug javascript, obligé de réassigner la valeur " " (testé sur firefox)
-        if (caractere === ' ') {
-            caractere = ' ';
-        } 
-        zoneDeTexte.innerHTML = zoneDeTexte.innerHTML + caractere;
-    }, 1/vitesseEcriture * 500 * i);
+      // bug javascript, obligé de réassigner la valeur " " (testé sur firefox)
+      if (caractere === " ") {
+        caractere = " ";
+      }
+      zoneDeTexte.innerHTML = zoneDeTexte.innerHTML + caractere;
+    }, (1 / vitesseEcriture) * 500 * i);
   }
+}
+
+function isANumber(data) {
+  let tabNum = "1234567890".split("");
+  let tabData = data.toString().split("");
+  let allNumbers = true;
+
+  if (allNumbers) {
+    for (let i = 0; i < data.length; i++) {
+      const car = tabData[i];
+
+      if (!tabNum.includes(car)) {
+        allNumbers = false;
+      }
+    }
+  }
+
+  return allNumbers;
 }
